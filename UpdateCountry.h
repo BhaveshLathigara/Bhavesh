113467f8b6e34bbdd23892328986879ebe013105
BhaveshLathigara <bhaveshsoni1318@yahoo.in>
2013-08-31 19:01:13 +0530

    Update UpdateCountry.h

--- UpdateCountry.h
+++ UpdateCountry.h
@@ -1,11 +1,12 @@
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
+#import "Countries.h"
 @interface UpdateCountry : UIViewController<NSFetchedResultsControllerDelegate>
 {
    IBOutlet UITextField *txtUpdateCountry;
-
+    Countries *objCountries;
 }
 @property (nonatomic, retain) IBOutlet UITextField *txtUpdateCountry;
-
+@property (nonatomic, retain) Countries *objCountries;
 -(IBAction)updateData:(id)sender;
 @end
