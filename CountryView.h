379f5466599baacd029e38c1f4faacf6f2e7b0da
BhaveshLathigara <bhaveshsoni1318@yahoo.in>
2013-08-31 18:53:45 +0530

    Update CountryView.h

--- CountryView.h
+++ CountryView.h
@@ -1,6 +1,8 @@
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
-
+#import "Countries.h"
+#import "StateView.h"
+#import "UpdateCountry.h"
 @interface CountryView : UIViewController<UITableViewDelegate,UITableViewDataSource,NSFetchedResultsControllerDelegate,UIAlertViewDelegate>
 
 {
@@ -8,14 +10,19 @@
     UIBarButtonItem *btnAddCountry;
 
     NSFetchedResultsController *countryFRC;
-
-
+    
+    Countries *objCountries;
+    StateView *objStateViewCall;
+    UpdateCountry *objUpdateCountry;
+    
 }
+
 @property (nonatomic,retain) IBOutlet UITableView *countryTableView;
 @property (nonatomic, retain) UIBarButtonItem *btnAddCountry;
 @property (nonatomic, retain) NSFetchedResultsController *countryFRC;
 
--(void)insertCountry;
 
+-(void)insertCountry;
+-(void) getCountry;
 
 @end
