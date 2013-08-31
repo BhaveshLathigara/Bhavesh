e45082cb9795df5181765cb356cef64832e3b0e0
BhaveshLathigara <bhaveshsoni1318@yahoo.in>
2013-08-31 19:00:17 +0530

    Update AddCountry.m

--- AddCountry.m
+++ AddCountry.m
@@ -1,140 +1,78 @@
+#import "AddCountry.h"
 #import "AppDelegate.h"
-#import "CountryView.h"
+#import "Countries.h"
+@implementation AddCountry
 
-@implementation AppDelegate
+AppDelegate *appDelegate;
 
-@synthesize window;
-@synthesize objCountryView;
-@synthesize navigationController;
-@synthesize managedObjectModel;
-@synthesize managedObjectContext;
-@synthesize persistentStoreCoordinator;
+@synthesize txtAddCountry;
 
-- (void)dealloc
-{
-    [window release];
-    [objCountryView release];
-    [super dealloc];
-}
+#pragma mark - View lifecycle
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (void)viewDidLoad
 {
-    self.objCountryView = [[[CountryView alloc] initWithNibName:@"CountryView" bundle:nil] autorelease];
-    self.navigationController = [[UINavigationController alloc] initWithRootViewController:self.objCountryView];
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    
-    self.window.backgroundColor = [UIColor whiteColor];
-    
-    [self.window makeKeyAndVisible];
-    
-    self.window.rootViewController = self.navigationController;
-
-    return YES;
-}
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-}
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
-}
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
-}
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-}
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    [self saveContext];
-}
--(void)saveContext
-{
-    NSError *error = nil;
-    NSManagedObjectContext *managedObjectContext1 = self.managedObjectContext;
-    if (managedObjectContext1 != nil)
-    {
-        if ([managedObjectContext1 hasChanges] && ![managedObjectContext1 save:&error])
-        {
-            
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            abort();
-        } 
-    }    
-}
-- (NSManagedObjectContext *)managedObjectContext
-{
-    if (managedObjectContext != nil)
-    {
-        return managedObjectContext;
-    }
-    
-    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-    if (coordinator != nil)
-    {
-        managedObjectContext = [[NSManagedObjectContext alloc] init];
-        [managedObjectContext setPersistentStoreCoordinator:coordinator];
-    }
-    return managedObjectContext;
+    [super viewDidLoad];
+    self.title = NSLocalizedString(@"Add Country", @"Add Country");
+    appDelegate = [AppDelegate sharedAppDelegate];
+    // Do any additional setup after loading the view from its nib.
 }
 
-- (NSManagedObjectModel *)managedObjectModel
+-(IBAction)saveData:(id)sender
 {
-    if (managedObjectModel != nil)
-    {
-        return managedObjectModel;
-    }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"CoreDatabaseConnectionAll" withExtension:@"momd"];
-    managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];    
-    return managedObjectModel;
-}
-
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
-    if (persistentStoreCoordinator != nil)
-    {
-        return persistentStoreCoordinator;
-    }
-    
-    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"CoreDatabaseConnectionAll.sqlite"];
-    
-    NSError *error = nil;
-    persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
-    {
+    if(txtAddCountry.text.length != 0)
+    {   
+        Countries *objCountries = 
+        [NSEntityDescription insertNewObjectForEntityForName:@"Countries"
+                                      inManagedObjectContext:appDelegate.managedObjectContext];
         
-        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
-    }    
+        
+        if (objCountries != nil){
+            
+            objCountries.cname = self.txtAddCountry.text;
+            
+            NSError *savingError = nil;
+
+            if ([appDelegate.managedObjectContext save:&savingError]){
+                
+                [self.navigationController popViewControllerAnimated:YES];
+                
+            }
+            else {
+                NSLog(@"Failed to save the managed object context.");
+            }
+            
+        } else {
+            NSLog(@"Failed to create the new person object.");
+        }
+    }
+    else
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Warning" message:@"Text Field should not be Blank." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+		[alert show];
+		[alert release];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
     
-    return persistentStoreCoordinator;
+    // Release any cached data, images, etc that aren't in use.
 }
 
-#pragma mark -
-#pragma mark MyMethods
 
-- (NSURL *)applicationDocumentsDirectory
+- (void)viewDidUnload
 {
-    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
 }
 
-+(AppDelegate *) sharedAppDelegate
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    return (AppDelegate *)[UIApplication sharedApplication].delegate;
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 @end
