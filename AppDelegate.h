c5efc2de5d3ce78d897bf5833f1503eab9b936df
BhaveshLathigara <bhaveshsoni1318@yahoo.in>
2013-08-31 18:32:01 +0530

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,22 @@
+#import <UIKit/UIKit.h>
+
+@class CountryView;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CountryView *objCountryView;
+
+@property (nonatomic, strong) UINavigationController *navigationController;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
